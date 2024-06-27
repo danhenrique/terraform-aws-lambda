4ba8831dc2a21850a559a6a4909cc059755ad81f
DanHenrique <daanielhenrique7@gmail.com>
2024-06-26 23:00:16 -0300

    fix: example

--- examples/infra/main.tf
+++ examples/infra/main.tf
@@ -1,12 +1,14 @@
-module "iam_role_with_policies" {
-  source               = "github.com/DanHenrique/terraform-aws-lambda?ref=v0.0.1"
+module "lambda" {
+  source               = "../../"
 
   # Lambda configuration
-  function_name            = "python-example_function"
-  function_handler = "lambda_function.lambda_handler"
   function_runtime = "python3.12"
+  function_code = "../dist/app.zip"
+  function_name            = "python-example_function"
+  function_description = "Python example function"
+  function_handler = "lambda_function.lambda_handler"
+
   function_role = "arn:aws:iam::211125374603:role/ExampleRole"
-  lambda_code_path = "../app"
   environment_variables = {}
   # CloudWatch configuration
   log_retention_days = 1
